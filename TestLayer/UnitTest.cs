96b7f43f4a094f892fcbfc553bec0dce7a031d31
Emre Geçer <unknown_ddos@hotmail.com>
2023-06-06 19:23:08 +0300

    update

--- TestLayer/UnitTest.cs
+++ TestLayer/UnitTest.cs
@@ -1,15 +1,25 @@
 using InternShipProject.Concrete;
+using InternShipProject.Interfaces;
 
 namespace TestLayer
 {
     public class UnitTest
     {
+        ICircleService circleService;
+        ITriangelService triangelService;
+        public UnitTest(ICircleService circleService, ITriangelService triangelService)
+        {
+            this.circleService = circleService;
+            this.triangelService = triangelService;
+
+                
+        }
         [Fact]
         public void Test_Circle_Area()
         {
-            var val = new CircleManager();
 
-            var area = val.AreaOfCircle(30); 
+           
+            var area = circleService.AreaOfCircle(30);
 
             Assert.Equal(188.4, area);
 
@@ -19,8 +29,8 @@ namespace TestLayer
         [Fact]
         public void Test_Triangel_Area()
         {
-            var val = new TriangelManager();
-            var area = val.AreaOfTriangel(4,13,15);
+         
+            var area = triangelService.AreaOfTriangel(4, 13, 15);
 
             Assert.Equal(24, area);
            
